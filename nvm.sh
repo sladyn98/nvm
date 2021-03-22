8488f81598b2ecca9c4fa49be28b0e8cd30858a6
Botspot <54716352+Botspot@users.noreply.github.com>
2021-03-22 15:49:08 -0500

    make if statement compatible with dash
    Co-authored-by: Jordan Harband <ljharb@gmail.com>

--- nvm.sh
+++ nvm.sh
@@ -1797,9 +1797,8 @@ nvm_get_arch() {
     *) NVM_ARCH="${HOST_ARCH}" ;;
   esac
   
-  #If running a 64bit ARM kernel but a 32bit ARM userland
-  #change NVM_ARCH to 32bit ARM (armv7l)
-  if [ "$NVM_ARCH" == arm64 ] && file "$(readlink -f "/sbin/init")" | grep -q 32;then
+  # If running a 64bit ARM kernel but a 32bit ARM userland, change NVM_ARCH to 32bit ARM (armv7l)
+  if [ "${NVM_ARCH}" = arm64 ] && file "$(readlink -f "/sbin/init")" | nvm_grep -q 32; then
     NVM_ARCH=armv7l
   fi
   
